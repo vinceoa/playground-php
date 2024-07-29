b21546d3db3271eda9e7352a7cc45b1734900620
Vince Scott <vince@openanswers.co.uk>
2024-07-29 16:50:28 +0100

    test

--- html/index.php
+++ html/index.php
@@ -1,3 +1,12 @@
-<?
-print("hello world 2");
+<?php
+session_start();
+$localPage = './menu/index.php';
+header("Location: $localPage");
+exit;
+?>
+<html lang="en">
+<body>hello</body>
+
+</html>
+
 
